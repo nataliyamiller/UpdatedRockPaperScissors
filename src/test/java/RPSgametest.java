ed69cb76bbce368bfe5712858d181c3d6f9eb9cc
Nataliya <nataliyamiller@Nataliyas-MacBook-Pro.local>
2015-08-12 20:42:08 -0700

    Fixed errors in FluentLenium test

--- src/test/java/RPSgametest.java
+++ src/test/java/RPSgametest.java
@@ -1,19 +1,22 @@
 
 
-import org.junit.*;
+
 import org.fluentlenium.adapter.FluentTest;
+import static org.junit.Assert.*;
+import org.junit.*;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import static org.junit.Assert.*;
+
 
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RPSgametest extends FluentTest{
+
   public WebDriver webDriver = new HtmlUnitDriver();
-  public WebDriver getDefailtDriver(){
+  public WebDriver getDefaultDriver() {
     return webDriver;
   }
 
@@ -85,7 +88,7 @@ public class RPSgametest extends FluentTest{
   public void isResultDisplayed() {
     goTo("http://localhost:4567/");
     fill("#userinput").with("Paper");
-    submit("#submit-button");
+    submit(".btn");
     assertThat(pageSource()).contains("result is");
   }
 }
